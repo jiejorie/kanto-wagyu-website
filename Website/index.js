2611b0ecd37d40cef8ca6f5d3899e03cbf3e984a
jiejorie <168099968+jiejorie@users.noreply.github.com>
2026-04-24 01:50:00 +0400

    Implement some of Offer Carousel

--- Website/index.js
+++ Website/index.js
@@ -7,3 +7,32 @@ toggle.addEventListener('click', () => {
     menu.classList.toggle('open');
     header.classList.toggle('nav-open');
 });
+
+// ===== OFFER CAROUSEL BUTTON JS =====
+
+// Declaring Global variables
+let scrollContainer = document.querySelector(".gallery")
+let backBtn = document.getElementById("back-btn");  
+let nextBtn = document.getElementById("next-btn");
+
+let backComputedStyle = window.getComputedStyle(backBtn);
+let nextComputedStyle = window.getComputedStyle(nextBtn);
+
+
+// Adding an event listener with the "wheel" event
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+})
+
+// Event listener that listens for a click from the Next Button, sliding left between the images
+nextBtn.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft += 460;
+})
+
+// Event listener that listens for a click from the Back Button, sliding right between the images
+backBtn.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft -= 460;
+})
