52871001ae2e7692c3e94a59bdabc4b253c0510d
jiejorie <168099968+jiejorie@users.noreply.github.com>
2026-05-02 00:50:33 +0400

    Add comments in JavaScript

--- Website/index.js
+++ Website/index.js
@@ -1,27 +1,37 @@
 // ===== TOGGLE BUTTON JS =====
+
+// Initialize navigation elements
 const toggle = document.getElementById('nav-toggle');
 const menu = document.getElementById('nav-links');
 const header = document.querySelector('header');
 
+// Listens for a click 
 toggle.addEventListener('click', () => {
     menu.classList.toggle('open');
     header.classList.toggle('nav-open');
 });
 
+
 // ===== NAV SCROLL =====
+
+// Listens for a scroll
 window.addEventListener('scroll', () => {
     const navbar = document.querySelector('.nav-bar');
 
-    if (window.scrollY > 50) { // When scrolled 50px
+    // Changes navigation bar appearance after scrolling 50px
+    if (window.scrollY > 50) { 
         navbar.classList.add('scrolled');
     } else {
         navbar.classList.remove('scrolled');
     }
 });
 
+
 // ===== MENU CATEGORIES FOOD FILTER =====
 
+// Filters menu
 function display_menu(menu_category) {
+    
     // Hides all categories with the class 'menu-group'
     const category = document.getElementsByClassName("menu-group");
     for (let i = 0; i < category.length; i++) {
@@ -35,37 +45,55 @@ function display_menu(menu_category) {
     }
 }
 
+
 // ===== OFFER CAROUSEL BUTTON JS =====
 
+// Initialize slide index
 let slideIndex = 1;
 showSlides(slideIndex);
 
+// Adds the slide index
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
+// Shows current slide index
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
+// Displays slides 
 function showSlides(n) {
+
+    // Initialize Carousel Elements
     let i;
     let slides = document.getElementsByClassName("promotion-offer");
     let dots = document.getElementsByClassName("dot");
+    
+    // Ensures slide index stays in range
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
+
+    // Hides previous slide
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
+
+    // Sets an active dot
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
+
+    // Shows current slide
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
 
+
 // ===== SUPPORT FAQs =====
+
 function toggleQuestion(btn) {
+
   //Find the parent FAQ container
   const question = btn.closest('.faq');
   const allQuestions = document.querySelectorAll('.faq');
@@ -79,6 +107,4 @@ function toggleQuestion(btn) {
 
   // Toggle the clicked question
   question.classList.toggle('show-text');
-
-  allQuestions.style.animation = "showAnswer 1s ease";
 }
